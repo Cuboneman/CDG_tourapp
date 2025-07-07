a0ec088da7ba76c6496fb8f4db50ff16eec78671
Cuboneman <edvin.matsson@gmail.com>
2025-07-07 22:15:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -99,3 +99,7 @@ def index():
         rounds.append({'id': rid, 'scores': scores})
 
     return render_template('index.html', players=players, rounds=rounds)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
